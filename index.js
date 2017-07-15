c4e0fe5ae2949d5ea029688faea8391388a27b81
lcb931023 <lcb931023@gmail.com>
2017-07-15 09:49:19 -0700

    spiral

--- index.js
+++ index.js
@@ -3,37 +3,40 @@ const randomColor = require('randomcolor');
 const paper = require('paper')
 
 paper.install(window);
-
-// calc rotation pos based on angle
-function rotX(angle) {
-  return Math.cos(angle)
-}
-function rotY(angle) {
-  return Math.sin(angle)
-}
-
 window.onload = function() {
+
   paper.setup('myCanvas');
   
-  const AMT = 500
+  const AMT = 5000
   
   const arrPoints = []
   
+  // total random connection
+  // for (var i = 0; i < AMT; i++) {
+  //   const p = new Point(
+  //     100 + Math.random() * 600,
+  //     100 + Math.random() * 400,      
+  //   )
+  //   arrPoints.push(p)
+  // }
+  
+  // spiral
   for (var i = 0; i < AMT; i++) {
     const p = new Point(
-      100 + Math.random() * 600,
-      100 + Math.random() * 400,      
+      400 + rotX(i * 0.5) * (i * 0.3),
+      300 + rotY(i * 0.5) * (i * 0.3),
     )
     arrPoints.push(p)
   }
   
   const path = new Path(arrPoints)
-  path.closed = true
+  // path.closed = true
   path.strokeColor = 'black'
   
   const SPEED = 2
   const RANGE = 10
   const OFFSET = 1
+  
 
   view.onFrame = function(event) {
     // On each frame, rotate the path by 3 degrees:
@@ -43,3 +46,7 @@ window.onload = function() {
     }
   }
 }
+
+// calc rotation pos based on angle
+function rotX(angle) { return Math.cos(angle) }
+function rotY(angle) { return Math.sin(angle) }
